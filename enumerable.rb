b6abe6e9b45bf4bb5d46b7d7f65a232546e47931
Luis Preza <preza.luis@gmail.com>
2019-10-07 18:34:21 -0500

    my_each

--- enumerable.rb
+++ enumerable.rb
@@ -1,1 +1,13 @@
-puts "hi"
+module Enumerable
+  def my_each
+    (self.length).times do |x|
+      yield(self[x], y = nil)
+    end
+  end
+end
+
+test = [5,2,7,4]
+
+test.my_each { |x|
+  puts x * 10
+}
