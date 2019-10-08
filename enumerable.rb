720eccfa6c177453989edbeb1c816c5600eff16c
Luis Preza <preza.luis@gmail.com>
2019-10-08 17:10:48 -0500

    sticlker errors fixed

--- enumerable.rb
+++ enumerable.rb
@@ -8,15 +8,15 @@ module Enumerable
   end
 
   def my_each_with_index
-    (self.length).times do |x|
+    length.times { |x|
       yield(x, self[x])
-    end
+    }
   end
 
   def my_select
     item = []
-    (self.length).times do |x|
-      if(yield(self[x], y = nil))
+    length.times do |x|
+      if yield(self[x])
         item.push(self[x])
       end
     end
@@ -24,21 +24,21 @@ module Enumerable
   end
 
   def my_all
-    (self.length).times do |x|
+    length.times do |x|
       return false unless yield(self[x])
     end
     true
   end
 
   def my_any
-    (self.length).times do |x|
+    length.times do |x|
       return true if yield(self[x])
     end
     false
   end
 
   def my_none
-    (self.length).times do |x|
+    length.times do |x|
       return false if yield(self[x])
     end
     true
@@ -46,8 +46,8 @@ module Enumerable
 
   def my_count
     item = 0
-    (self.length).times do |x|
-      if(yield(self[x], y = nil))
+    length.times do |x|
+      if yield(self[x], y = nil)
         item += 1
       end
     end
@@ -56,19 +56,15 @@ module Enumerable
 
   def my_map(my_proc = false)
     item = []
-    (self.length).times do |x|
+    length.times do |x|
       if my_proc
         result = my_proc.call(self[x])
       else
         result = yield(self[x])
       end
-      if(result)
-        item.push(true)
-      else
-        item.push(false)
-      end
+      item.push(result)
     end
-    return item
+    item
   end
 
   def my_inject
@@ -76,15 +72,11 @@ module Enumerable
     (self.length - 1).times do |x|
       sum = yield(sum, self[x + 1])
     end
-    return sum
+    sum
   end
 
   def multiply_els
-    self.my_inject { |sum, n| sum * n }
+    my_inject { |sum, n| sum * n }
   end
 
 end
-test = [1, 2, 3]
-test.my_each { |x|
-  puts x + 10
-}
